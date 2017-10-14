6a7d6dcee28679351e90ef9959f77b96553caee0
Gudianbu <30794287+Gudianbu@users.noreply.github.com>
2017-10-14 14:00:08 +0800

    UI页面完善，商品、种类、库存数据访问类完善

---
+++ BBBTeaSS.BLL/ProductBLL.cs
@@ -0,0 +1,32 @@
+﻿using BBBTeaSS.Model;
+using BBBTeaSS.DAL;
+using MateralTools.MResult;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MateralTools.MEncryption;
+
+namespace BBBTeaSS.BLL
+{
+    /// <summary>
+    /// 商品业务逻辑层
+    /// </summary>
+    public class ProductBLL
+    {
+        /// <summary>
+        /// 数据访问对象
+        /// </summary>
+        private ProductDAL productDAL;
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public ProductBLL()
+        {
+            productDAL = new ProductDAL();
+        }
+
+
+    }
+}
